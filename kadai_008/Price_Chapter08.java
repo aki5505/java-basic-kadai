df4e1767f32c00bb8456c65eb18a181e4a6f96cf
aki <akki.s1102@gmail.com>
2024-06-30 13:07:09 +0900

    kadai_008を修正

--- kadai_008/Price_Chapter08.java
+++ kadai_008/Price_Chapter08.java
@@ -5,23 +5,45 @@ public class Price_Chapter08 {
     public static void main(String[] args) {
     	
         int userAge = 30;
-        String serviceCost = "";
+        int serviceCost;
         
         switch (userAge) {
             case 10:
-                serviceCost = "10代の料金は1000円";
+                serviceCost = 1000;
+                System.out.println("10代の料金は" + serviceCost + "円");
                 break;
             case 20:
-                serviceCost = "20代の料金は2000円";
+            	serviceCost = 2000;
+                System.out.println("20代の料金は" + serviceCost + "円");
                 break;
             case 30:
+            	serviceCost = 3000;
+                System.out.println("30代の料金は" + serviceCost + "円");
+                break;
             case 40:
-                serviceCost = "30代・40代の料金は3000円";
+            	serviceCost = 3000;
+                System.out.println("40代の料金は" + serviceCost + "円");
+                break;
+            case 50:
+            	serviceCost = 4000;
+                System.out.println("50代の料金は" + serviceCost + "円");
+                break;
+            case 60:
+            	serviceCost = 4000;
+            	System.out.println("60代の料金は" + serviceCost + "円");
+            	break;
+            case 70:
+            	serviceCost = 4000;
+                System.out.println("70代の料金は" + serviceCost + "円");
+                break;
+            case 80:
+            	serviceCost = 5000;
+            	System.out.println("80代の料金は" + serviceCost + "円");
                 break;
             default:
-                serviceCost = "それ以外は500円";
+            	serviceCost = 500;
+                System.out.println("それ以外は" + serviceCost + "円");
                 break;
         }
-        System.out.println(serviceCost);
     }
 }
