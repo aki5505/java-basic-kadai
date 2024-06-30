16fbc80a286f2cd2456458f90fa0960b2c81ddb7
aki <akki.s1102@gmail.com>
2024-06-30 23:38:46 +0900

    test

--- kadai_017/KatoExec_Chapter17.java
+++ kadai_017/KatoExec_Chapter17.java
@@ -5,14 +5,15 @@ public class KatoExec_Chapter17 {
     public static void main(String[] args) {
 
         KatoTaro_Chapter17 Taro = new KatoTaro_Chapter17();
-        Taro.setGivenName();
-        Taro.execIntroduce();
 
         KatoIchiro_Chapter17 Ichiro = new KatoIchiro_Chapter17();
-        Ichiro.setGivenName();
-        Ichiro.execIntroduce();
 
         KatoHanako_Chapter17 Hanako = new KatoHanako_Chapter17();
+
+        Taro.setGivenName();
+        Taro.execIntroduce();
+        Ichiro.setGivenName();
+        Ichiro.execIntroduce();
         Hanako.setGivenName();
         Hanako.execIntroduce();
 
